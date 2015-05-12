a34dd021cf7dfcfd29616fc32d2aeb824db33176
root <risul.islam@regfire.com>
2015-05-12 10:18:58 +0200

    Updated to slick.js 1.5.3

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
     name: 'risul:slick',
     summary: "Slick - the last carousel you'll ever need",
-    version: "1.5.0_5",
+    version: "1.5.3",
     git: "https://github.com/risul/meteor-slick"
 });
 
